b242e44aea0026b06a7f0740899b7a81075b5848
Asadrayat <asad.rayat4@gmail.com>
2022-10-11 14:21:35 +0600

    api loaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Component/Blog/Blog';
 import Root from './Component/Root/Root';
+import Statistic from './Component/Statistic/Statistic';
+import Home from './Component/Home/Home';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Root></Root> 
+      element: <Root></Root> ,
+      children : [
+        {
+          path : "/",
+          loader :() => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element : <Home></Home>,
+        },
+        {
+          path : "/home",
+          element : <Home></Home>,
+        },
+        {
+          path : "/blog",
+          element : <Blog></Blog>,
+        },
+        {
+          path : "/statistic",
+          element : <Statistic></Statistic>,
+        },
+      ]
     },
   ]);
   return (
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='container mx-auto mt-4 '>
-            <nav className='flex justify-between px-12 py-6 bg-sky-200 '>
+            <nav className='flex justify-between px-12 py-6  bg-indigo-600 '>
                 <div>
-                    <p className="font-bold	text-4xl text-blue-800">What A Quiz!</p>
+                    <p className="font-bold	text-4xl text-white">What A Quiz!</p>
                 </div>
-                <div className='font-bold flex gap-8 text-2xl text-blue-600  '>
-                    <Link className='hover:text-blue-900' to ="/">Topic</Link>
-                     <Link className='hover:text-blue-900'  to ="/blog">Blog</Link>
-                    <Link className='hover:text-blue-900' to ="/statistic">Statistic</Link>
+                <div className='font-bold flex gap-8 text-2xl text-white  '>
+                    <Link className='hover:text-gray-400' to="/">Home</Link>
+                    <Link className='hover:text-gray-400' to="/blog">Blog</Link>
+                    <Link className='hover:text-gray-400' to="/statistic">Statistic</Link>
                 </div>
             </nav>
         </div>
