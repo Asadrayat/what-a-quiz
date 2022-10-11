020591f34cfedbb24600e137e2d55286f90a1b71
Asadrayat <asad.rayat4@gmail.com>
2022-10-11 15:24:43 +0600

    eror page added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Blog from './Component/Blog/Blog';
 import Root from './Component/Root/Root';
 import Statistic from './Component/Statistic/Statistic';
 import Home from './Component/Home/Home';
+import Error from './Component/Error/Error';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root> ,
+      errorElement : <Error></Error>,
       children : [
         {
           path : "/",
-          loader :() => fetch('https://openapi.programming-hero.com/api/quiz'),
           element : <Home></Home>,
+          loader :() => fetch('https://openapi.programming-hero.com/api/quiz'),
         },
         {
           path : "/home",
--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <p>this blog</p>
+            
         </div>
     );
 };
